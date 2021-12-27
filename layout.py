0b658d94217b4939b21e4c7c9e57e35a2d8e29e9
RishiLinux <bhavani24Bv@gmail.com>
2021-12-27 12:07:03 +0530

    Added layout.py

---
+++ layout.py
@@ -0,0 +1,5 @@
+from typing import List  # noqa: F401
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
