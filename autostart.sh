9ee40754e4836ca68fb4fb72cfa88c77b308265f
RishiLinux <bhavani24Bv@gmail.com>
2021-12-29 21:23:43 +0530

    Added spectacle for future

--- autostart.sh
+++ autostart.sh
@@ -11,4 +11,7 @@ imwheel -b 45
 # natural scroll
 xinput set-button-map 8 1 2 3 5 4 6 7 8 9 10
 
+# pending
+# screenshot tool spectacle from kde here -l is to just launch and -b is for to launch in background
+# spectacle -l -b 
 
