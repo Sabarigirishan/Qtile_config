3830c7e5ab7f0bf138d4cf452a8855f225d33793
RishiLinux <bhavani24Bv@gmail.com>
2022-01-10 16:01:15 +0530

    Modified screenshot shells

--- screenshot_w.sh
+++ screenshot_w.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir ~/Pictures/Screenshots/`date +%m-%Y` -p && 
 scrot -u -p -C --file ~/Pictures/Screenshots/try.png && 
-xclip < ~/Pictures/Screenshots/try.png && 
+xclip -selection clipboard -t image/png ~/Pictures/Screenshots/try.png && 
 mv ~/Pictures/Screenshots/try.png ~/Pictures/Screenshots/`date +%m-%Y`/Screenshot_`date +%H:%M:%S_%d-%m-%Y`.png
