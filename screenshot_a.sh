795fcea1674cf2ff5309021f1a9ee022c30fcd1e
RishiLinux <bhavani24Bv@gmail.com>
2022-01-10 20:22:05 +0530

    Removed some bugs

--- screenshot_a.sh
+++ screenshot_a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir ~/Pictures/Screenshots/`date +%m-%Y` -p && 
-scrot -s mode=edge --pointer -f ~/Pictures/Screenshots/try.png && 
+scrot -l mode=edge, -s --pointer ~/Pictures/Screenshots/try.png && 
 xclip -selection clipboard -t image/png ~/Pictures/Screenshots/try.png && 
 mv ~/Pictures/Screenshots/try.png ~/Pictures/Screenshots/`date +%m-%Y`/Screenshot_`date +%H:%M:%S_%d-%m-%Y`.png
