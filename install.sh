3db7ff26506e150e401c0b39e8649a348e631302
RishiLinux <bhavani24Bv@gmail.com>
2022-01-17 22:23:01 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# yay
-sudo pacman -S git base-devel 
+echo "This script is currently for arch-linux, yay is necessary."
+sleep 2
 
-git clone http://aur.archlinux.org/yay-git.git && 
-    cd yay git &&
-    mkaepkg -si
-
-# update
 echo "want to update?"
 echo "(y/n):" 
 read update
 
 if [[ $update == 'y' || $update == 'Y' ]]
 then 
+    echo "======================================================================================"
+    echo "Installing updating"
+    echo "======================================================================================"
+    sleep 1
+
     yay -Syu
     echo "Want to reboot?"
     echo "(y/n):"
@@ -28,16 +28,37 @@ then
 fi
 
 echo "======================================================================================"
+echo "Installing base-devel(imp)"
+echo "======================================================================================"
+sleep 1
+sudo pacman -S git base-devel --needed
+
+echo "Want to install yay?"
+echo "(y/n):" 
+read y_install
+
+if [[ $y_install == 'y' || $y_install == 'Y' ]]
+then 
+    echo "======================================================================================"
+    echo "Installing yay (imp)"
+    echo "======================================================================================"
+    sleep 1
+    git clone http://aur.archlinux.org/yay-git.git && 
+        cd yay git &&
+        mkaepkg -si
+fi
+
+echo "======================================================================================"
 echo "Installing base install (imp)"
 echo "======================================================================================"
 sleep 1
 yay -S xf86-video-ati lib32-mesa mandb xorg xorg-init picom-tryone-git kitty polkit acpi --needed
 
 echo "======================================================================================"
-echo "Installing fonts and emoji"
+echo "Installing packages related to fonts and emoji"
 echo "======================================================================================"
 sleep 1
-yay -S ttf-nerd-fonts-symbols ttf-nerd-fonts-icons ttf-twemoji ttf-hack noto-font-emoji --needed
+yay -S ttf-nerd-fonts-symbols ttf-nerd-fonts-icons ttf-twemoji ttf-hack noto-font-emoji x11-emoji-picker-git --needed
 
 echo "======================================================================================"
 echo "Installing terminal based packages"
@@ -56,6 +77,69 @@ echo "Installing packages for entertainment purposes"
 echo "======================================================================================"
 sleep 1
 yay -S smplayer spotify telegram qbittorrent smtube --needed
+
+echo "======================================================================================"
+echo "Installing optional packages(These are really optional types)"
+echo "======================================================================================"
+sleep 1
+
+echo "Want to install following packages (y/n):"
+echo ">ttf-ms-fonts:"
+read optional_1
+if [[ $optional_1 == 'y' || $optional_1 == 'Y' ]]
+then
+    yay -S ttf-ms-fonts --needed
+fi
+
+echo "======================================================================================"
+echo "Installing WM"
+echo "======================================================================================"
+sleep 1
+
+echo ">Select WM:"
+echo "1. Awesome"
+echo "2. Qtile"
+echo "3. I3"
+echo "4. dwm"
+echo "5. Skip"
+read WM
+case "$WM" in
+    "1") yay -S awesome --needed
+    ;;
+    "2") yay -S qtile --needed
+    ;;
+    "3") yay -S i3 --needed
+    ;;
+    "4") yay -S dwm --needed
+    ;;
+    "5") echo "Skipping."
+esac
+
+echo "======================================================================================"
+echo "Installing DE"
+echo "======================================================================================"
+sleep 1
+
+echo ">Select WM:"
+echo "1. Gnome"
+echo "2. Plasma"
+echo "3. Deepin"
+echo "4. Mate"
+echo "5. Skip"
+read DE
+case "$DE" in
+    "1") yay -S gnome --needed
+    ;;
+    "2") yay -S plasma-desktop sddm --needed
+    ;;
+    "3") yay -S deepin --needed
+    ;;
+    "4") yay -S mate --needed
+    ;;
+    "5") echo "Skipping."
+esac
+
+
 
 echo "======================================================================================"
 echo "Cleaning all cache and unused packages (won't cause much issue)"
